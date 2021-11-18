e2a36c9a169f812f85a67ffd883318dc31d306ee
prasys <pradeesh@prasys.info>
2021-11-19 12:10:04 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ setup(
     author='',
     author_email='',
     download_url='',
-
     packages=[''],
     package_dir={'': '.'}, # the . is important else package_data option gets ignored 
    package_data={'': ['_example.so','_example']},
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'Operating System :: MacOS :: MacOS X',
+       'Operating System :: POSIX :: Linux', # WSL will be treated as Linux so it's not a problem
+   ],
 
 )
