0ee4b2fca1dd16ca79c4cc6851a4c9df28809c87
prasys <pradeesh@prasys.info>
2021-11-19 10:34:54 +1300

    added initial support for mac

--- makepypi.sh
+++ makepypi.sh
@@ -3,6 +3,9 @@ cd build
 cmake ..
 make
 mv example.py ..
-mv _example.so ..
+if [ "$(uname)" == "Darwin" ]; then
+    #mv _example.so ..
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    mv _example.so
 touch ../setup.cfg # dummy file for now - but will use it for config os platform
 python3 ../setup.py sdist #
--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, Extension
 setup(
     name='PACKAGENAME',
     version='PACKAGEVERSION',  # specified elsewhere
+    author='',
+    author_email='',
+    download_url='',
+
     packages=[''],
     package_dir={'': '.'}, # the . is important else package_data option gets ignored 
-   package_data={'': ['_example.so']},
+   package_data={'': ['_example.so','_example']},
 
 )
