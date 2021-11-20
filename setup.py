a7d500546471255c29a6677531f8b7cf1cd79c0b
prasys <pradeesh@prasys.info>
2021-11-21 12:03:22 +1300

    updated code to include binary distr

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, Extension
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
+
 setup(
     name='PACKAGENAME',
     version='PACKAGEVERSION',  # specified elsewhere
@@ -7,11 +12,13 @@ setup(
     download_url='',
     packages=[''],
     package_dir={'': '.'}, # the . is important else package_data option gets ignored 
-   package_data={'': ['_example.so','_example']},
+   package_data={'': ['_example.so']},
    classifiers=[
        'Programming Language :: Python :: 3',
        'Operating System :: MacOS :: MacOS X',
        'Operating System :: POSIX :: Linux', # WSL will be treated as Linux so it's not a problem
    ],
+   python_requires='>=3.5'
+   distclass=BinaryDistribution
 
 )
